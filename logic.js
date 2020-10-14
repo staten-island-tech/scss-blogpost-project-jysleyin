e59fe53e409699c889441c476a4ffbfa182c8a35
jysleyin <julissaliangg@gmail.com>
2020-10-14 11:23:07 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,44 +1,44 @@
-// $(document).ready(function () {
-//   // Add smooth scrolling to all links
-//   $("a").on("click", function (event) {
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== "") {
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-//       // define
-//       var hash = this.hash;
+      // define
+      var hash = this.hash;
 
-//       //  800 milliseconds
-//       $("html, body").animate(
-//         {
-//           scrollTop: $(hash).offset().top,
-//         },
-//         800,
-//         function () {
-//           window.location.hash = hash;
-//         }
-//       );
-//     }
-//   });
-// });
+      //  800 milliseconds
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
 
-// //set variable
-// var mybutton = document.getElementById("myBtn");
+//set variable
+var mybutton = document.getElementById("myBtn");
 
-// window.onscroll = function () {
-//   scrollFunction();
-// };
+window.onscroll = function () {
+  scrollFunction();
+};
 
-// // create function. this will tell us that at 20px it will appear.
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
+// create function. this will tell us that at 20px it will appear.
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
